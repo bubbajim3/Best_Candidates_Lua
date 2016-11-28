c75103b65385cb93d69ac8f2b29ed5bc7ea79a00
James A. Donnell Jr <bubbajim715@verizon.net>
2016-11-28 14:58:50 -0500

    Minor changes

--- src/best_candidates.lua
+++ src/best_candidates.lua
@@ -1,14 +1,18 @@
--- Sample run
+-- Sample run 1
 local function main()
-  testVoter = {1, 1, 1, 0, 0, -1}
-  testCandidates = {{"Clinton", 1, 1, 1, -1, 0, 0}, {"Trump", 0, 1, 1, -1, -1, -1}}
+  local testVoter = {1, 0, 0, 0}
+  local testCandidates = 
+    {{"Bill", -1, -1, -1 ,-1},
+    {"Ronald", 0, 0, 0, 0},
+    {"Bill Nye", 1, -1, 1, -1},
+    {"Red", 1, 1, 1, 1}}
   show(best_candidates(testVoter, testCandidates))
 end
 
 -- Score candidate in respect to voter.
 -- Returns their score
-function scoreCandidate(voter, candidate)
-  score = 0
+local function scoreCandidate(voter, candidate)
+  local score = 0
   for i=1, #voter  do
     if(voter[i]==candidate[i+1] and voter[i]~=0) then
       score = score + 1
@@ -21,8 +25,8 @@ end
 
 -- Score all candidates
 -- Returns array of {{Name, Score}, {Name, Score}...}
-function scoreCandidates(voter, candidates)
-  candidateScores = {}
+local function scoreCandidates(voter, candidates)
+  local candidateScores = {}
   for i=1, #candidates do
     candidateScores[i] = {candidates[i][1], scoreCandidate(voter, candidates[i])}
   end
@@ -30,8 +34,8 @@ function scoreCandidates(voter, candidates)
 end
 
 -- Finds the highest score amongst all candidates
-function highestScore(candidateScores)
-  score = candidateScores[1][2]
+local function highestScore(candidateScores)
+  local score = candidateScores[1][2]
   for i=2, #candidateScores do
     if(candidateScores[i][2]>score) then
       score = candidateScores[i][2]
@@ -42,8 +46,8 @@ end
 
 -- Retrieves the candidate(s) with the highest score
 -- Returns an array of names only
-function highestCandidates(score, candidateScores)
-  candidates = {}
+local function highestCandidates(score, candidateScores)
+  local candidates = {}
   for i=1, #candidateScores do
     if(candidateScores[i][2]==score) then
       candidates[#candidates+1] =  candidateScores[i][1]
@@ -62,7 +66,7 @@ end
 -- Finds the best candidate(s) for a given Voter
 -- Returns an array of candidate name(s)
 function best_candidates(voter, candidates)
-  candidateScores = scoreCandidates(voter, candidates)
+  local candidateScores = scoreCandidates(voter, candidates)
   return highestCandidates(highestScore(candidateScores), candidateScores)
 end
 
